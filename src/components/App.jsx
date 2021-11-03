7e85b66129e1c17d3a10757bd1aa0a02e58e5942
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-03 23:15:24 +0100

    button attributes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import React, { useState } from "react";
 function App() {
   return (
     <div>
-      <h1></h1>
-      <button>+</button>
-      <button>-</button>
-      <button>X</button>
+      <h1>{}</h1>
+      <button onClick="increase">+</button>
+      <button onClick="decrease">-</button>
+      <button onClick="times">X</button>
     </div>
   );
 }
