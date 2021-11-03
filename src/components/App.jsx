4fb23549fcd86cd409e150d02f88d18cea3cfc7a
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-03 23:12:33 +0100

    new edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,4 @@
 import React, { useState } from "react";
-function App() {
-  let [count, changeCount] = useState(1);
 
-  function sub() {
-    changeCount(count--);
-  }
-
-  function add() {
-    changeCount(count++);
-  }
-
-  function multiply() {
-    changeCount(count * 2);
-  }
-
-  function remainder() {
-    changeCount(count % 3);
-  }
-
-  function divide() {
-    changeCount(count / 2);
-  }
-
-  return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={sub}>-</button>
-      <button onClick={add}>+</button>
-      <button onClick={multiply}>x</button>
-      <button onClick={remainder}>%</button>
-      <button onClick={divide}>/</button>
-    </div>
-  );
-}
+function App() {}
 export default App;
