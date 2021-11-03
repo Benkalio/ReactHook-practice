182a60d62613e60f0268af2c9c3bb15ad9bfd5d2
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-03 23:16:40 +0100

    count

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div>
-      <h1>{}</h1>
+      <h1>{count}</h1>
       <button onClick="increase">+</button>
       <button onClick="decrease">-</button>
       <button onClick="times">X</button>
