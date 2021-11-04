c2ed69cacff6f2bdb5363733acae211845eb8bd1
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-04 17:02:47 +0100

    implementation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div>
       <h1>{}</h1>
-      <button onClick={}></button>
-      <button onClick={}></button>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
       <button onClick={}></button>
     </div>
   );
