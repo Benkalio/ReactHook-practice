b4debc3999c8eac04d3ac89ac39a0b606968e8f5
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-05 23:18:15 +0100

    reiterate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,16 @@
 import React, { useState } from "react";
 
 function App() {
-  let [counter, changeCounter] = useState(10);
+  let [count, setCount] = useState(12);
 
   function increase() {
-    changeCounter(counter++);
+    setCount(count++);
   }
 
   function decrease() {
-    changeCounter(counter--);
+    setCount(count--);
   }
 
-  function multiply() {
-    changeCounter(counter * 3);
-  }
-  return (
-    <div>
-      <h1>{counter}</h1>
-      <h2>
-        Implementing React useState method to change the state of this App
-      </h2>
-      <button onClick={increase}>+</button>
-      <button onClick={decrease}>-</button>
-      <button onClick={multiply}>x</button>
-    </div>
-  );
+  return <div></div>;
 }
 export default App;
