57eadda389fb9abb4e94a0bf7f66eae372ede311
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-05 23:20:21 +0100

    decount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 
 function App() {
-  let [count, setCount] = useState(12);
+  let [count, setCount, deCount] = useState(12);
 
   function increase() {
     setCount(count++);
+    deCount(count * 3);
   }
 
   function decrease() {
     setCount(count--);
   }
 
-  return <div></div>;
+  return (
+    <div>
+      <h1>{count}</h1>
+      <h2>{deCount}</h2>
+    </div>
+  );
 }
 export default App;
